9ce690a9a5e09f8cc7bf435ea27a6b6e1d8a880e
Infarh <shmachilin@yandex.ru>
2021-06-20 00:24:19 +0300

    Ресурсы кистей

---
+++ MathCore.WPF.Styles/AppStyles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace MathCore.WPF.Styles
+{
+    public partial class AppStyles
+    {
+        public AppStyles()
+        {
+            MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/MathCore.WPF.Styles;component/Styles/CheckBoxes/SwitcherCheckBox.xaml") });
+            MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/MathCore.WPF.Styles;component/Styles/TextBoxes/TaggedTextBox.xaml") });
+            MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/MathCore.WPF.Styles;component/Resources/EasingFunctions.xaml") });
+            MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/MathCore.WPF.Styles;component/Resources/Colors/Brushes.xaml") });
+        }
+    }
+}
